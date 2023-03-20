8182b83981971131f667a7f451e506289dd9f943
DarkDesignX <denis.basler@gmx.ch>
2023-03-20 20:44:44 +0100

    begin: login function backend

--- server/public/index.php
+++ server/public/index.php
@@ -19,25 +19,56 @@
 	$app->setBasePath("/server");
 
 	$app->post("/Login", function (Request $request, Response $response, $args) {
-		$data = json_decode(file_get_contents("php://input"), true);
-
-		require "util/config.php";
-		if (!$data || !isset($data["username"]) || !isset($data["password"]) || $data["username"] != $api_username || $data["password"] != $api_password) {
-			http_response_code(401);
-			die("Invalid credentials.");
+		// reads the requested JSON body
+		$body_content = file_get_contents("php://input");
+		$JSON_data = json_decode($body_content, true);
+	
+		// if JSON data doesn't have these then there is an error
+		if (isset($JSON_data["user_name"]) && isset($JSON_data["user_password"])) {
+		} else {
+			error_function(400, "Empty request");
 		}
-
-		//Generate the access token and store it in a cookie.
-		$token = Token::create($data["username"], $data["password"], time() + 3600, "localhost");
-		setcookie("token", $token, time() + 3600);
-
-		echo "Success.";
-
+	
+		// Prepares the data to prevent bad data, SQL injection andCross site scripting
+		$user_name = validate_string($JSON_data["user_name"]);
+		$user_password = validate_string($JSON_data["user_password"]);
+	
+			if (!$user_password) {
+				error_function(400, "user_password is invalid, must contain at least 5 characters");
+			}
+			if (!$user_name) {
+				error_function(400, "user_name is invalid, must contain at least 5 characters");
+			}
+	
+		$user_password = hash("sha256", $user_password);
+	
+		$user = get_user_by_user_name($user_name);
+	
+		if ($user["user_password"] !==  $user_password) {
+			error_function(404, "not Found");
+		}
+	
+		$token = create_token($user_name, $user_password, $user["ID"]);
+	
+		setcookie("token", $token);
+	
+		message_function(200, "Succesfuly created Token");
+	
 		return $response;
 	});
-
-	// require "routes/product.php";
-	// require "routes/category.php";
+	
+	function user_validation()
+	{
+		$current_user = validate_token();
+		if ($current_user === false) {
+			error_function(403, "unauthenticated");
+		}
+		return $current_user;
+	}
+	
+	// require_once "Controler/validation.php";
+	// require_once "Controler/error-and-info-messages.php";
+	// require_once "Model/SQL.php";
 
 	$app->run();
 ?>
