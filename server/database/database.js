c10357951029c71b237928346e7b3d5b231486ab
DarkDesignX <denis.basler@gmx.ch>
2023-03-23 10:47:18 +0100

    user functions api working

--- server/database/database.js
+++ server/database/database.js
@@ -2,13 +2,13 @@ let pool = null;
 
 const initializeMariaDB = async () => {
   const mariadb = require("mariadb");
-  const config = {
+  pool = mariadb.createPool({
     database: process.env.DB_NAME || "mychat",
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "supersecret123",
     connectionLimit: 5,
-  };
+  });
 
 };
 
