eda480bdc0b86d963e08c0a3c775fbe277dfd9e1
TeoMunoz <teo.munoz.blasco@gmail.com>
2023-03-24 13:11:54 +0100

    websockrd V1.0

--- client/send-menssage.js
+++ client/send-menssage.js
@@ -2,14 +2,29 @@ const sendButton = document.getElementById('send-m');
 const messageInput = document.getElementById('menssage');
 const chatMessages = document.getElementById('chat');
 
+const socket = new WebSocket('wss://loclahost:3000');
+
+socket.addEventListener('open', () => {
+  console.log('work');
+});
+
+socket.addEventListener('message', (event) => {
+  const message = document.createElement('div');
+  message.classList.add('container-chat', 'bot');
+  message.innerHTML = `<p class="text-bot">${event.data}</p>`;
+  chatMessages.insertBefore(message, null);
+});
 
 function sendMessage() {
-  console.log('Botón enviar funciona');
+  console.log('work:)');
   if (messageInput.value.trim() !== '') {
+    socket.send(messageInput.value);
+
     const message = document.createElement('div');
     message.classList.add('container-chat', 'iam');
     message.innerHTML = `<p class="text-iam">${messageInput.value}</p>`;
     chatMessages.insertBefore(message, null);
+    
     messageInput.value = '';
   }
 }
