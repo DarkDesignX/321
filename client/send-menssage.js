137d26867d3226f3b5cfb227b2fff698484d7920
TeoMunoz <teo.munoz.blasco@gmail.com>
2023-03-24 10:51:27 +0100

    send update

--- client/send-menssage.js
+++ client/send-menssage.js
@@ -1,14 +1,24 @@
 const sendButton = document.getElementById('send-m');
 const messageInput = document.getElementById('menssage');
-const chatMessages = document.getElementById('chat-messages');
+const chatMessages = document.getElementById('chat');
 
-sendButton.addEventListener('click', () => {
+
+function sendMessage() {
   console.log('Botón enviar funciona');
   if (messageInput.value.trim() !== '') {
     const message = document.createElement('div');
     message.classList.add('container-chat', 'iam');
     message.innerHTML = `<p class="text-iam">${messageInput.value}</p>`;
-    chatMessages.appendChild(message);
+    chatMessages.insertBefore(message, null);
     messageInput.value = '';
+  }
+}
+
+sendButton.addEventListener('click', sendMessage);
+
+messageInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    sendMessage();
   }
 });
